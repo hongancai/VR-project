a5f894d7cbdf58c88aaf275beeb801d71366fac5
Hong An Cai <w44793458@gmail.com>
2024-06-07 15:23:28 +0800

    BALL Speed Test

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    public GameObject soccerPrefab; // 球的预制件
+    public GameObject soccerPrefab; 
     public Transform spawnPoint; // 生成球的位置
-    public float gameDuration = 180f; // 游戏持续时间（秒）
+    public float gameDuration = 180f; 
 
     private bool isGameOver = false;
     private bool isBallDestroyed = false;
@@ -46,16 +46,16 @@ public class GameManager : MonoBehaviour
         while (!isGameOver && Time.time - startTime < gameDuration)
         {
             SpawnBall();
-            yield return new WaitForSeconds(3f); // 每3秒生成一颗球
+            yield return new WaitForSeconds(4f); 
         }
 
         isGameOver = true;
-        StopAllBalls(); // 结束游戏时停止所有球的运动
+        StopAllBalls(); 
     }
 
     public void RespawnBall()
     {
-        if (!isGameOver && !isBallDestroyed) // 当游戏未结束且球未被摧毁时重新生成球
+        if (!isGameOver && !isBallDestroyed) 
         {
             StartCoroutine(SpawnBallWithDelay());
         }
@@ -63,7 +63,7 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator SpawnBallWithDelay()
     {
-        yield return new WaitForSeconds(1f); // 延迟1秒再生成新的球
+        yield return new WaitForSeconds(2f); // 延遲2秒再生成新的球
         SpawnBall();
     }
 
@@ -94,8 +94,8 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        isGameOver = false; // 重新开始游戏时重置游戏状态
-        isBallDestroyed = false; // 重新开始游戏时重置球的状态
-        StartCoroutine(GameLoop()); // 重新开始游戏循环
+        isGameOver = false; 
+        isBallDestroyed = false; 
+        StartCoroutine(GameLoop()); 
     }
 }
